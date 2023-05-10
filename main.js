a0ee8e62f9df6adde5fed5a77affda73fd6f7a48
Jazmine <jazmihic@amazon.com>
2023-05-10 15:25:46 -0500

    added dropdown

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 console.log("Hello World");
-fetch(
-  "https://api.sportsdata.io/v3/cfb/scores/json/Teams?key=50f62ae419ea48078a9625733360b999"
-)
+fetch("http://localhost:3000/footballteams")
   .then((response) => response.json())
   .then((json) => console.log(json));
+let dropdown = document.getElementById("football-teams");
+dropdown.length = 0;
+let defaultOption = document.createElement("option");
+defaultOption.text = " Select Team";
+dropdown.add(defaultOption);
+dropdown.selectedIndex = 0;
+const url = "http://localhost:3000/footballteams";
+fetch(url).then(function (response) {
+  return response.json().then(function (data) {
+    let option;
+
+    for (let i = 0; i < data.length; i++) {
+      option = document.createElement("option");
+      option.text = data[i].name;
+      option.value = data[i].abbreviation;
+      dropdown.add(option);
+    }
+  });
+});
