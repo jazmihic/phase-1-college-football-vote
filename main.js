d74ba58696e76d3cd620681db6b57e9ff11243a0
Jazmine <jazmihic@amazon.com>
2023-05-10 14:01:02 -0500

    adding drop down

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 console.log("Hello World");
-fetch("http://localhost:3000/footballteams")
+fetch(
+  "https://api.sportsdata.io/v3/cfb/scores/json/Teams?key=50f62ae419ea48078a9625733360b999"
+)
   .then((response) => response.json())
   .then((json) => console.log(json));
