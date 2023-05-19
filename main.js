22e2dbc28be5d2383a140b0db901f6a6a40f55fe
Jazmine <jazmihic@amazon.com>
2023-05-19 14:45:54 -0500

    changed api input

--- main.js
+++ main.js
@@ -2,22 +2,24 @@ console.log("Hello World");
 fetch("http://localhost:3000/footballteams")
   .then((response) => response.json())
   .then((json) => console.log(json));
-let dropdown = document.getElementById("football-teams");
-dropdown.length = 0;
-let defaultOption = document.createElement("option");
-defaultOption.text = " Select Team";
-dropdown.add(defaultOption);
-dropdown.selectedIndex = 0;
-const url = "http://localhost:3000/footballteams";
-fetch(url).then(function (response) {
-  return response.json().then(function (data) {
-    let option;
 
-    for (let i = 0; i < data.length; i++) {
-      option = document.createElement("option");
-      option.text = data[i].name;
-      option.value = data[i].abbreviation;
-      dropdown.add(option);
-    }
+const init = () => {
+  const inputForm = document.querySelector("form");
+
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = document.querySelector("input#searchByTeam");
+
+    console.log(input.value);
+
+    fetch(
+      "https://api.sportsdata.io/v3/cfb/scores/json/Teams?key=50f62ae419ea48078a9625733360b999"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
   });
-});
+};
+
+document.addEventListener("DOMContentLoaded", init);
